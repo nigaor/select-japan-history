b99371e30660015109ff4355c756d48fe3fac038
nigaor <ktnkuhi0116@gmail.com>
2025-07-02 01:25:37 +0900

    feat: 7/2 コードのフォーマット

--- src/app/components/EventCard.tsx
+++ src/app/components/EventCard.tsx
@@ -12,16 +12,20 @@ const EventCard: React.FC<EventCardProps> = ({ event }) => {
   if (!event) {
     return (
       <div className="w-full h-full flex items-center justify-center bg-gray-100 rounded-lg">
-        <p className="text-gray-500">年表にカーソルを合わせると、出来事が表示されます。</p>
+        <p className="text-gray-500">
+          年表にカーソルを合わせると、出来事が表示されます。
+        </p>
       </div>
     );
   }
 
   return (
     <div className="bg-white p-6 rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold mb-2">{event.name} ({event.year}年)</h2>
+      <h2 className="text-2xl font-bold mb-2">
+        {event.name} ({event.year}年)
+      </h2>
       <p className="text-gray-600 mb-4">場所: {event.locationName}</p>
-      
+
       {event.image && (
         <div className="relative w-full h-60 mb-4">
           <Image
@@ -33,7 +37,7 @@ const EventCard: React.FC<EventCardProps> = ({ event }) => {
           />
         </div>
       )}
-      
+
       <p className="text-gray-800">{event.description}</p>
     </div>
   );
--- src/app/components/JapanMap.tsx
+++ src/app/components/JapanMap.tsx
@@ -4,38 +4,33 @@ import React from "react";
 import { MapContainer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import JapanGeoJson from "@/data/japan.json";
-import type { GeoJsonObject } from 'geojson';
-import type { PathOptions, StyleFunction } from 'leaflet';
-
+import type { GeoJsonObject } from "geojson";
+import type { StyleFunction } from "leaflet";
 
 interface JapanMapProps {
   highlightedLocationId: number | null;
 }
 
 const JapanMap: React.FC<JapanMapProps> = ({ highlightedLocationId }) => {
-
   const style: StyleFunction = (feature) => {
     const isHighlighted = highlightedLocationId === feature?.properties.id;
     return {
       fillColor: isHighlighted ? "#F56565" : "#E2E8F0",
       weight: 0.5,
       opacity: 1,
-      color: 'white',
-      fillOpacity: 1
+      color: "white",
+      fillOpacity: 1,
     };
   };
 
   return (
     <MapContainer
-      center={[36, 135.5]}
+      center={[39, 139]}
       zoom={5}
-      style={{ height: "500px", width: "100%", backgroundColor: 'white' }}
+      style={{ height: "500px", width: "100%", backgroundColor: "white" }}
       zoomControl={false}
       attributionControl={false}
     >
-      {/* 背景のタイルレイヤーは指定しないことで、白地図にする。
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" /> 
-      */}
       <GeoJSON data={JapanGeoJson as GeoJsonObject} style={style} />
     </MapContainer>
   );
